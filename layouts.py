d9a59ddbd4ac340a8121799a0b004afa69714e1c
Pascal Corpet <pcorpet@users.noreply.github.com>
2020-02-16 09:52:48 +0100

    Fix large indentations by breaking line earlier. (#63)

--- layouts.py
+++ layouts.py
@@ -98,17 +98,19 @@ def create_parameters_layout(name, suffix='', input_drone=_POSITIONS[0], style=N
 
         dbc.Container([
             html.H3(_('Simulation ') + name),
-            html.P(html.Small(html.I(_('You can get additional info on parameters '
-                                       'descriptions by running your mouse '
-                                       'over each of them.')))),
+            html.P(html.Small(html.I(_(
+                'You can get additional info on parameters descriptions by running your mouse over '
+                'each of them.')))),
             dbc.Col([
                 html.H6(_('Drone parameters')),
 
-                html.Div([html.Label(_('Initial drone location'), id="ini_pos"),
-                          dbc.Tooltip(_('Where drones are stationed. The simulator selects '
-                                        'the closest available drone (as the crow flies)'),
-                                      target="ini_pos",
-                                      placement="left")]),
+                html.Div([
+                    html.Label(_('Initial drone location'), id="ini_pos"),
+                    dbc.Tooltip(
+                        _('Where drones are stationed. The simulator selects the closest available '
+                          'drone (as the crow flies)'),
+                        target="ini_pos",
+                        placement="left")]),
                 dcc.Dropdown(
                     id=f'input_drone{suffix}',
                     # TODO(pascal): Translate labels here.
@@ -116,48 +118,55 @@ def create_parameters_layout(name, suffix='', input_drone=_POSITIONS[0], style=N
                     value=input_drone,
                 ),
 
-                html.Div([html.Label(_('Max drone speed (in km/h)'), id="speed_e"),
-                          dbc.Tooltip(_('Maximum horizontal speed'),
-                                      target="speed_e",
-                                      placement="left")]),
+                html.Div([
+                    html.Label(_('Max drone speed (in km/h)'), id="speed_e"),
+                    dbc.Tooltip(
+                        _('Maximum horizontal speed'),
+                        target="speed_e",
+                        placement="left")]),
                 dbc.Input(id=f'speed{suffix}', value='80', type='text'),
 
-                html.Div([html.Label(_("Drone's acceleration time (in sec):"), id="acc_e"),
-                          dbc.Tooltip(_('Time needed for the drone to reach its '
-                                        'maximum horizontal speed.'),
-                                      target="acc_e",
-                                      placement="left")]),
+                html.Div([
+                    html.Label(_("Drone's acceleration time (in sec):"), id="acc_e"),
+                    dbc.Tooltip(
+                        _('Time needed for the drone to reach its maximum horizontal speed.'),
+                        target="acc_e",
+                        placement="left")]),
                 dbc.Input(id=f'acc{suffix}', value='5', type='text'),
 
-                html.Div([html.Label(_("Drone's vertical speed (in m/s):"), id="vert-acc_e"),
-                          dbc.Tooltip(_('Maximum vertical speed. It is assumed that the time '
-                                        'needed for the drone to reach this speed is negligible'),
-                                      target="vert-acc_e",
-                                      placement="left")]),
+                html.Div([
+                    html.Label(_("Drone's vertical speed (in m/s):"), id="vert-acc_e"),
+                    dbc.Tooltip(
+                        _('Maximum vertical speed. It is assumed that the time needed for the '
+                          'drone to reach this speed is negligible'),
+                        target="vert-acc_e",
+                        placement="left")]),
                 dbc.Input(id=f'vert-acc{suffix}', value='9', type='text'),
 
-                html.Div([html.Label(_("Drone's cruise altitude (in m):"), id="alt_e"),
-                          dbc.Tooltip(_('Horizontal cruise altitude'),
-                                      target="alt_e",
-                                      placement="left")]),
+                html.Div([
+                    html.Label(_("Drone's cruise altitude (in m):"), id="alt_e"),
+                    dbc.Tooltip(
+                        _('Horizontal cruise altitude'),
+                        target="alt_e",
+                        placement="left")]),
                 dbc.Input(id=f'alt{suffix}', value='100', type='text'),
 
-                html.Div([html.Label(_('Unavailability of the drone after a run (in h):'),
-                                     id="unav_e"),
-                          dbc.Tooltip(_('time needed after a run for the '
-                                        'drone to be available again. It accounts for the '
-                                        'time spent on the OHCA '
-                                        'location and the time of refurbishment and '
-                                        'rehabilitation of equipment.'),
-                                      target="unav_e",
-                                      placement="left")]),
+                html.Div([
+                    html.Label(_('Unavailability of the drone after a run (in h):'), id="unav_e"),
+                    dbc.Tooltip(
+                        _('time needed after a run for the drone to be available again. It '
+                          'accounts for the time spent on the OHCA location and the time of '
+                          'refurbishment and rehabilitation of equipment.'),
+                        target="unav_e",
+                        placement="left")]),
                 dbc.Input(id=f'unavail_delta{suffix}', value='6', type='text'),
 
-                html.Div([html.Label(_('Drone can fly during aeronautical night'), id="day_e"),
-                          dbc.Tooltip(_('Whether the drone can only fly '
-                                        'during the aeronautical day or not'),
-                                      target="day_e",
-                                      placement="left")]),
+                html.Div([
+                    html.Label(_('Drone can fly during aeronautical night'), id="day_e"),
+                    dbc.Tooltip(
+                        _('Whether the drone can only fly during the aeronautical day or not'),
+                        target="day_e",
+                        placement="left")]),
                 dcc.RadioItems(
                     id=f'day{suffix}',
                     options=[
@@ -170,76 +179,85 @@ def create_parameters_layout(name, suffix='', input_drone=_POSITIONS[0], style=N
 
                 html.H6(_('Operational parameters')),
 
-                html.Div([html.Label(_("Delay at departure (in s):"), id="dep_e"),
-                          dbc.Tooltip(_('Delay at departure needed for the operator to set up '
-                                        'flight information for the drone then for the fire '
-                                        'station where the drone '
-                                        'is stationed to launch it.'),
-                                      target="dep_e",
-                                      placement="left")]),
+                html.Div([
+                    html.Label(_("Delay at departure (in s):"), id="dep_e"),
+                    dbc.Tooltip(
+                        _('Delay at departure needed for the operator to set up flight information '
+                          'for the drone then for the fire station where the drone is stationed to '
+                          'launch it.'),
+                        target="dep_e",
+                        placement="left")]),
                 dbc.Input(id=f'dep_delay{suffix}', value='15', type='text'),
 
-                html.Div([html.Label(_("Delay on arrival (in s):"), id="arr_e"),
-                          dbc.Tooltip(_('Delay on arrival needed for the drone to narrow '
-                                        'its landing and for '
-                                        'the bystander to catch the AED.'),
-                                      target="arr_e",
-                                      placement="left")]),
+                html.Div([
+                    html.Label(_("Delay on arrival (in s):"), id="arr_e"),
+                    dbc.Tooltip(
+                        _('Delay on arrival needed for the drone to narrow its landing and for '
+                          'the bystander to catch the AED.'),
+                        target="arr_e",
+                        placement="left")]),
                 dbc.Input(id=f'arr_delay{suffix}', value='15', type='text'),
 
-                html.Div([html.Label(_("Delay between detection of unconsciousness"
-                                       " and OHCA detection (in s):"),
-                                     id="del_e"),
-                          dbc.Tooltip(_('Mean time spent between unconsciousness '
-                                        'and OHCA detection by '
-                                        'emergency call dispatchers. Unconsciousness detection '
-                                        'activates BLS teams whereas drones are activated '
-                                        'only at OHCA detection.'),
-                                      target="del_e",
-                                      placement="left")]),
+                html.Div([
+                    html.Label(
+                        _("Delay between detection of unconsciousness  and OHCA detection (in s):"),
+                        id="del_e"),
+                    dbc.Tooltip(
+                        _('Mean time spent between unconsciousness and OHCA detection by '
+                          'emergency call dispatchers. Unconsciousness detection activates BLS '
+                          'teams whereas drones are activated only at OHCA detection.'),
+                        target="del_e",
+                        placement="left")]),
                 dbc.Input(id=f'detec_delay{suffix}', value='104', type='text'),
 
-                html.Div([html.Label(_('Rate of OHCA at home, which are detected by call '
-                                       'center operators '
-                                       '(between 0 and 1):'),
-                                     id="deth_e"),
-                          dbc.Tooltip(_('When the OHCA is not detected by the '
-                                        'emergency dispatchers no drone is sent.'),
-                                      target="deth_e",
-                                      placement="left")]),
+                html.Div([
+                    html.Label(
+                        _('Rate of OHCA at home, which are detected by call center operators '
+                          '(between 0 and 1):'),
+                        id="deth_e"),
+                    dbc.Tooltip(
+                        _('When the OHCA is not detected by the emergency dispatchers no drone is '
+                          'sent.'),
+                        target="deth_e",
+                        placement="left")]),
                 dbc.Input(id=f'detec_rate_home{suffix}', value='0.8', type='text'),
 
-                html.Div([html.Label(_('Rate of OHCA in the streets, which are '
-                                       'detected by call center operators '
-                                       '(between 0 and 1):'),
-                                     id="dets_e"),
-                          dbc.Tooltip(_('When the OHCA is not detected by the '
-                                        'emergency dispatchers no drone is sent.'),
-                                      target="dets_e",
-                                      placement="left")]),
+                html.Div([
+                    html.Label(
+                        _('Rate of OHCA in the streets, which are detected by call center '
+                          'operators (between 0 and 1):'),
+                        id="dets_e"),
+                    dbc.Tooltip(
+                        _('When the OHCA is not detected by the emergency dispatchers no drone is '
+                          'sent.'),
+                        target="dets_e",
+                        placement="left")]),
                 dbc.Input(id=f'detec_rate_vp{suffix}', value='0.12', type='text'),
 
-                html.Div([html.Label(_("Rate of OHCA at home, which only have one "
-                                       "witness alone (between 0 and 1):"),
-                                     id="wit_e"),
-                          dbc.Tooltip(_('The simulation requires at least '
-                                        'two witnesses for OHCA at home : '
-                                        'one to stay near the victim, the other to '
-                                        'go out in the street to get the AED brought by drone.'),
-                                      target="wit_e",
-                                      placement="left")]),
+                html.Div([
+                    html.Label(
+                        _("Rate of OHCA at home, which only have one witness alone (between 0 and "
+                          "1):"),
+                        id="wit_e"),
+                    dbc.Tooltip(
+                        _('The simulation requires at least two witnesses for OHCA at home : one '
+                          'to stay near the victim, the other to go out in the street to get the '
+                          'AED brought by drone.'),
+                        target="wit_e",
+                        placement="left")]),
                 dbc.Input(id=f'wit_detec{suffix}', value='0.58', type='text'),
 
             ], style={'flex': 1}),
 
-            dbc.Button(id=f'seq_start{suffix}', n_clicks=0,
-                       children=_('Update simulation'), block='center',
-                       style={'flex': 1}),
+            dbc.Button(
+                id=f'seq_start{suffix}', n_clicks=0,
+                children=_('Update simulation'), block='center',
+                style={'flex': 1}),
 
         ], style={'flex': 1} if style is None else dict(style, flex=1)),
-        dbc.Col(children=create_graphs_layout(suffix=suffix,
-                                              style={'display': 'flex', 'flex': 'initial'}),
-                )
+        dbc.Col(children=create_graphs_layout(
+            suffix=suffix,
+            style={'display': 'flex', 'flex': 'initial'}))
     ], style={'display': 'flex', 'flex': 'auto'})
 
 
@@ -281,9 +299,9 @@ def create_both_graphs(name, suffix='', style=None):
 def create(lang):
     lang = gettext.translation('messages', localedir='locales', languages=[lang], fallback=True)
     lang.install()
-    return dbc.Container(
-        children=[dbc.Container(className='title',
-                                children=[html.H1(_('Airborne AED simulation'))]),
-                  dbc.Container(id='vp-control-tabs', className='control-tabs',
-                                children=[create_tabs_layout()],),
-                  ], )
+    return dbc.Container(children=[
+        dbc.Container(
+            className='title', children=[html.H1(_('Airborne AED simulation'))]),
+        dbc.Container(
+            id='vp-control-tabs', className='control-tabs', children=[create_tabs_layout()],),
+    ])
