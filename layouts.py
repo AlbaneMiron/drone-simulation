6b555c11c7d8572c1ff1ebbb13b73e971ae1d73f
AlbaneMiron <albane.delespinay@gmail.com>
2020-02-17 11:56:13 +0100

    fix again line too long

--- layouts.py
+++ layouts.py
@@ -50,7 +50,8 @@ def create_tabs_layout():
                             html.Img(
                                 src="https://maps.googleapis.com/maps/api/staticmap?center=Paris,"
                                 "+France&zoom=11&scale=false&size=600x300&maptype=roadmap&key="
-                                "AIzaSyAMs0JsrC88jq_yxCxfFqZ8dIBt0wEl3CY&format=png&visual_refresh=true",
+                                "AIzaSyAMs0JsrC88jq_yxCxfFqZ8dIBt0wEl3CY&format=png&"
+                                "visual_refresh=true",
                             ),
                         ], style={'text-align': 'center'}),
                         # dbc.Card(
