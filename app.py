5b1456a76d7e60c53353786fd1d73a758a5eb3cc
AlbaneMiron <albane.delespinay@gmail.com>
2020-02-17 13:09:11 +0100

    fix hyphens

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ app = dash.Dash(  # pylint: disable=invalid-name
     external_stylesheets=[
         dbc.themes.CERULEAN,
         {
-            'href': 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/"
-            "4.7.0/css/font-awesome.min.css',
+            'href': 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/'
+            '4.7.0/css/font-awesome.min.css',
             'rel': 'stylesheet',
             # 'integrity': 'sha384-50oBUHEmvpQ+1'
             # 'lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf',
             # 'crossorigin': 'anonymous'
         },
         {
-            'href': 'https://cdnjs.cloudflare.com/ajax/libs/"
-            "github-fork-ribbon-css/0.2.3/gh-fork-ribbon.min.css',
+            'href': 'https://cdnjs.cloudflare.com/ajax/libs/'
+            'github-fork-ribbon-css/0.2.3/gh-fork-ribbon.min.css',
             'rel': 'stylesheet',
         },
         {
